030aa3d6ebbfde863f5d3faeebf85639f82ee917
Julian Schäfer <julianschaefer2206@gmail.com>
2019-03-03 19:38:42 +0100

    migration name fix. also fixed last commit

--- database/migrations/2019_03_01_193846_create_sensor_values_table.php
+++ database/migrations/2019_03_01_193846_create_sensor_values_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDeviceSensorValuesTable extends Migration
+class CreateSensorValuesTable extends Migration
 {
     /**
      * Run the migrations.
